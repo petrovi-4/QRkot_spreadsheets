5a734b93fd80d6c745a2f72c5f4934db9879fa77
petrovi4 <40.10.444@gmail.com>
2023-07-29 14:48:23 +0300

    Version for reviev 1.1

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from app.constants import (  # noqa
-    G_LOCALE, G_TIME_FORMAT, G_VERSION_SHEETS, G_VERSION_DRIVE,  # noqa
-    G_SHEET_ID, G_TITLE_CLOSING_SPEED, G_ROW_COUNT, G_COLUMN_COUNT  # noqa
+    G_LOCALE,
+    G_VERSION_SHEETS,
+    G_TIME_FORMAT,
+    G_VERSION_DRIVE,  # noqa
+    G_SHEET_ID,
+    G_TITLE_CLOSING_SPEED,
+    G_ROW_COUNT,
+    G_COLUMN_COUNT,  # noqa
 )  # noqa
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,12 +1,11 @@
-from .donation import (
-    DonationCreate,  # noqa
-    CutDonationsDB,  # noqa
-    ExtDonationsDB,  # noqa
-)  # noqa
 from .charity_project import (
     CharityProjectCreate,  # noqa
     CharityProjectUpdate,  # noqa
     CharityProjectDB,  # noqa
 )  # noqa
-
+from .donation import (
+    DonationCreate,  # noqa
+    CutDonationsDB,  # noqa
+    ExtDonationsDB,  # noqa
+)  # noqa
 from .user import UserRead, UserCreate, UserUpdate  # noqa
