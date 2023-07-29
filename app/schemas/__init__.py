dfb58cfe6e21f746e856e5dfc53506f7dad4c6a4
petrovi4 <40.10.444@gmail.com>
2023-07-29 17:32:43 +0300

    Version for review 1.2

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,1 @@
-from .charity_project import (CharityProjectCreate,  # noqa
-    CharityProjectUpdate, CharityProjectDB  # noqa
-)  # noqa
-from .donation import (DonationCreate,  # noqa
-    CutDonationsDB, ExtDonationsDB  # noqa
-)  # noqa
-from .user import UserRead, UserCreate, UserUpdate  # noqa
+
